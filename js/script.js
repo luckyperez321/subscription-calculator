cf57eeead669b0082c00480612bc3e1c804a1132
Lucky Perez <luckyperez321@gmail.com>
2023-12-22 13:00:59 -0600

    sending folder for help to Lisa

--- js/script.js
+++ js/script.js
@@ -1,1 +1,12 @@
 // Your JS code here
+var subTypeElement = document.querySelector("#subscription");
+var subDurationElement = document.querySelector("#months");
+var result = document.querySelector(".result");
+var subType = "basic";
+var subDuration = 1; 
+ 
+subTypeElement.addEventListener("change",function (e) {
+subType = e.target.value;
+console.log(subType);
+});
+// Updating on Github to try to get help with console log.
